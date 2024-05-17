62996d0885b401d7be6bbd37e144479001064de3
chiaraiurato <chiara.iurat@gmail.com>
2024-05-17 19:18:43 +0200

    Added Jacoco
    Now it report on browser coverage of dumy with 75% (:

--- dummy/src/test/java/org/apache/zookeeper/AppTest.java
+++ dummy/src/test/java/org/apache/zookeeper/AppTest.java
@@ -6,16 +6,22 @@ import static org.junit.Assert.assertEquals;
 
 
 public class AppTest {
-        /**
-         * a simple test
-         */
-        @Test
-        public void sum()
-        {
-            App app = new App();
-            int c = app.sum(12, 4);
-            assertEquals(16, c);
+    /**
+     * a simple test
+     */
+    @Test
+    public void sum()
+    {
+        App app = new App();
+        int c = app.sum(12, 4);
+        assertEquals(16, c);
 
-        }
+    }
+    @Test
+    public void sub(){
+        App app = new App();
+        int c = app.sub(12,4);
+        assertEquals(c, 8);
+    }
 }
 
